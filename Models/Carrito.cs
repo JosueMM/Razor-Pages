815248d6460d291257109be837d64d43075f2e80
Roque Chacon Corrales <roke12chacon@gmail.com>
2018-11-21 20:05:41 -0600

    Repair model Carrito

--- Models/Carrito.cs
+++ Models/Carrito.cs
@@ -5,11 +5,10 @@ namespace  Razor_Page.Models
     public class Carrito
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Usuario { get; set; }
-        public string Correo { get; set; }
-        public string Contrasenna { get; set; }
-        public char Activo { get; set; }
-        public int Admin { get; set; }
+        public Usuario Usuario { get; set; }
+        public Producto Producto { get; set; }
+        public int Cantidad { get; set; }
+        public float Precio { get; set; }
+
     }
 }
